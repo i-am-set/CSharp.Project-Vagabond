3a4be630a3206f437124337a4f4e17c47fabeb61
Seth Gran <138339656+i-am-set@users.noreply.github.com>
2025-06-26 22:31:51 -0400

    ...

--- Core/Core.cs
+++ Core/Core.cs
@@ -15,10 +15,9 @@ using System;
 // TODO: Add a way to save and load the game state
 // TODO: Impliment dialogue logic into dialogue scene
 // TODO: Impliment combat logic into combat scene
-// TODO: Finish entity implimentation
 // TODO: Convert display to have larger map, smaller terminal, with the terminal input being hidden unless the player presses ~ to "open console"
 // TODO: Make it so clicking on the map sets a path to that location, but if a path is queued and you click again, it clears the path and makes a new one to that position instead of appending; then, if control clicking, it appends to the path; and lastly if alt clicked, it does the staright line "less moves" path
-// TODO: Make 1x 2x 3x speed mods under clock
+// TODO: Finish entity implimentation
 
 namespace ProjectVagabond
 {
